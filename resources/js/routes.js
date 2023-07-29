5e18bd9996581318af2a768428d54f83fd26fe4e
franklinVillegas <091236villegas@gmail.com>
2023-07-29 17:17:04 -0500

    refactor

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -14,33 +14,22 @@ import Inicio from './components/pages/Inicio';
 //admin
 import MainAdmin from './components/layouts/MainAdmin';
 import InicioAdmin from './components/admin/Inicio';
-import Usuarios from './components/admin/Usuarios';
-import InformacionDocente from './components/admin/InformacionDocente';
 
 
 ///Gestion
 import RecepcionCV from './components/admin/RecepcionCV';
 import Grado from './components/admin/Grado';
 import Seccion from './components/admin/Seccion';
-import Curso from './components/admin/Curso';
-import Actividad from './components/admin/Actividad';
 import Datos from './components/admin/Datos';
 import Matricula from './components/admin/Matricula';
 import Notas from './components/admin/Notas';
-import Asistencia from './components/admin/Asistencia';
 
 //Alumno
 import MainAlumno from './components/layouts/MainAlumno';
-import InicioAlumno from './components/alumno/Inicio';
 import DatosAlumno from './components/alumno/Datos';
-import NotasAlumno from './components/alumno/Notas';
 
 //Docente
 import MainDocente from './components/layouts/MainDocente';
-import InicioDocente from './components/docente/Inicio';
-import Evaluacion from './components/docente/Evaluacion';
-import RpteAsistencia from './components/docente/RpteAsistencia';
-import RpteNotas from './components/docente/RpteNotas';
 
 
 //Campus
@@ -48,9 +37,6 @@ import MainCampus from './components/layouts/MainCampus';
 import InicioCampus from './components/campus/Inicio';
 import BibliotecaVirtual from './components/campus/BibliotecaVirtual';
 import ContenidoDigital from './components/campus/ContenidoDigital';
-import Libro from './components/campus/Libro';
-import Video from './components/campus/Video';
-import SalaReunion from './components/campus/SalaReunion';
 import ReunionVirtual from './components/campus/ReunionVirtual';
 import EvaluacionVirtual from './components/campus/EvaluacionVirtual';
 import IniciarEvaluacionVirtual from './components/campus/IniciarEvaluacionVirtual';
@@ -94,74 +80,20 @@ let routes = [
     },
 
     {
-        path: '/alumno',
-        component: MainAlumno,
-        meta: { requiresAuthAlumno: true },
-        redirect: '/alumno/inicio',
-        children: [
-            { path: 'inicio', name: 'InicioAlumno', component: InicioAlumno },
-            { path: 'datosalumno', name: 'DatosAlumno', component: DatosAlumno },
-            { path: 'matricula', name: 'MatriculaAlumno', component: Matricula },
-            { path: 'asistencia', name: 'AsistenciaAlumno', component: Asistencia },
-            { path: 'notas', name: 'NotasAlumno', component: NotasAlumno},
-        ]
-    },
-
-    {
-        path: '/docente',
-        component: MainDocente,
-        meta: { requiresAuthDocente: true },
-        redirect: '/docente/inicio',
-        children: [
-            { path: 'inicio', name: 'InicioDocente', component: InicioDocente },
-            { path: 'asistencia', name: 'AsistenciaDocente', component: Asistencia },
-            { path: 'notas', name: 'NotasDocente', component: Notas },
-            { path: 'salaReunion', name: 'SalaReunionDocente', component: SalaReunion },
-            { path: 'evaluacion', name: 'EvaluacionDocente', component: Evaluacion },
-            { path: 'reporteAsistencia', name: 'RpteAsistencia', component: RpteAsistencia },
-            { path: 'reporteNotas', name: 'RpteNotas', component: RpteNotas },
-        ]
-    },
-
-    {
-        path: '/campus',
-        component: MainCampus,
-        meta: { requiresAuthCampus: true },
-        redirect: '/campus/inicio',
-        children: [
-            { path: 'inicio', name: 'InicioCampus', component: InicioCampus },
-            { path: 'bibliotecaVirtual', name: 'BibliotecaVirtual', component: BibliotecaVirtual },
-            { path: 'contenidoDigital', name: 'ContenidoDigital', component: ContenidoDigital },
-            { path: 'reunionVirtual', name: 'ReunionVirtual', component: ReunionVirtual },
-            { path: 'evaluacionVirtual', name: 'EvaluacionVirtual', component: EvaluacionVirtual },
-            { path: 'iniciarEvaluacionVirtual', name: 'IniciarEvaluacionVirtual', component: IniciarEvaluacionVirtual },
-            { path: 'evaluacion', name: 'EvaluacionPregunta', component: EvaluacionPregunta },
-        ]
-    },
-
-    {
         path: '/admin',
         component: MainAdmin,
         meta: { requiresAuthAdmin: true },
         redirect: '/admin/inicio',
         children: [
             { path: 'inicio', name: 'InicioAdmin', component: InicioAdmin },
-            { path: 'usuarios', name: 'Usuarios', component: Usuarios },
-            // { path: 'mensajes/:usuario_id/:nombre', name: 'Mensajes', component: Mensajes },
+            
             { path: 'recepciocv', name: 'RecepcionCV', component: RecepcionCV },
             { path: 'grado', name: 'Grado', component: Grado },
             { path: 'seccion', name: 'Seccion', component: Seccion },
-            { path: 'curso', name: 'Curso', component: Curso },
-            { path: 'competencia', name: 'Actividad', component: Actividad },
             { path: 'datos', name: 'Datos', component: Datos },
             { path: 'matricula', name: 'Matricula', component: Matricula },
             { path: 'notas', name: 'Notas', component: Notas },
-            { path: 'asistencia', name: 'Asistencia', component: Asistencia },
-            { path: 'informacionDocente', name: 'InformacionDocente', component: InformacionDocente },
-            { path: 'libro', name: 'Libro', component: Libro },
-            { path: 'video', name: 'Video', component: Video },
-            { path: 'salaReunion', name: 'SalaReunion', component: SalaReunion },
-            { path: 'evaluacion', name: 'Evaluacion', component: Evaluacion },
+            // { path: 'evaluacion', name: 'Evaluacion', component: Evaluacion },
             
  
         ]
