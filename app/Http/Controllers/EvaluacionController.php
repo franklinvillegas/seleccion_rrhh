caa325acf0197bb3719f5121dbc0d21d4ef3c8f4
franklin <091236villegas@gmail.com>
2023-08-09 09:34:07 -0500

    recepcion

--- app/Http/Controllers/EvaluacionController.php
+++ app/Http/Controllers/EvaluacionController.php
@@ -20,31 +20,30 @@ class EvaluacionController extends Controller
      public function ver($dni){
         $persona = Persona::select('id','nombres','apellido_pat','apellido_mat','documento' )->where('documento',$dni)->get();
         $proceso = PersonaConvocatoria::select('id','id_persona','id_convocatoria','id_sede_provincial')
-        ->where('id_persona',$persona[0]['id'])->latest()
-        ->first();
+        ->where('id_persona',$persona[0]['id'])->latest('id')->first();
         $user = auth()->user();
-            $id_region_user = DB::select("select sr.id from sede_regional sr RIGHT JOIN sede_provincial sp on sr.id = sp.id_sede_regional where sp.id=".$user->id_sede_provincial);            
-            $id_region_proceso = DB::select("select sr.id from sede_regional sr RIGHT JOIN sede_provincial sp on sr.id = sp.id_sede_regional where sp.id=". $proceso->id_sede_provincial);
-            if ($id_region_user==$id_region_proceso) {
-                $duplicado = Evaluacion::where('id_persona_convocatoria',$proceso->id)->first();
-                if ($duplicado) {
-                    $provincia = SedeProvincial::where('id',$proceso->id_sede_provincial)->first();
-                    $resultado = Evaluacion::select('num_registro')->where('id_persona_convocatoria',$proceso->id)->first();
-                    return response()->json(['message' => 'Ya esta registrado', 'persona' => $persona, 'proceso'=>$proceso,'num_registro'=>$resultado->num_registro,'provincia'=>$provincia, 'flag'=>0]);
-                }
-                else {
-                    $resultado = DB::select("select MAX(num_registro) as max_num_registro from persona_convocatoria pc 
-                    INNER JOIN evaluacion e ON pc.id = e.id_persona_convocatoria
-                    WHERE pc.id_convocatoria = ".$proceso->id_convocatoria." and pc.id_sede_provincial = ".$proceso->id_sede_provincial);
-                    $provincia = SedeProvincial::where('id',$proceso->id_sede_provincial)->first();
-                    $evaluacion = Evaluacion::create(['id_persona_convocatoria' => $proceso->id, 'num_registro' => $resultado[0]->max_num_registro+1,'estado'=> 0 ,]);
-                    return response()->json(['message' => 'Se realizo el registro', 'persona' => $persona, 'proceso'=>$proceso,'num_registro'=>$evaluacion->num_registro,'provincia'=>$provincia,'flag'=>1]);
-                }
-            } 
+        $id_region_user = DB::select("select sr.id from sede_regional sr RIGHT JOIN sede_provincial sp on sr.id = sp.id_sede_regional where sp.id=".$user->id_sede_provincial);            
+        $id_region_proceso = DB::select("select sr.id from sede_regional sr RIGHT JOIN sede_provincial sp on sr.id = sp.id_sede_regional where sp.id=". $proceso->id_sede_provincial);
+        if ($id_region_user==$id_region_proceso) {
+            $duplicado = Evaluacion::where('id_persona_convocatoria',$proceso->id)->first();
+            if ($duplicado) {
+                $provincia = SedeProvincial::where('id',$proceso->id_sede_provincial)->first();
+                $resultado = Evaluacion::select('num_registro')->where('id_persona_convocatoria',$proceso->id)->first();
+                return response()->json(['message' => 'Ya esta registrado', 'persona' => $persona, 'proceso'=>$proceso,'num_registro'=>$resultado->num_registro,'provincia'=>$provincia, 'flag'=>0]);
+            }
             else {
-                return response()->json(['message' => 'El postulante no petenece a la sede Regional', 'persona' => $persona, 'proceso'=>$proceso,'flag'=>1]);
-            }                
-    }
+                $resultado = DB::select("select MAX(num_registro) as max_num_registro from persona_convocatoria pc 
+                INNER JOIN evaluacion e ON pc.id = e.id_persona_convocatoria
+                WHERE pc.id_convocatoria = ".$proceso->id_convocatoria." and pc.id_sede_provincial = ".$proceso->id_sede_provincial);
+                $provincia = SedeProvincial::where('id',$proceso->id_sede_provincial)->first();
+                $evaluacion = Evaluacion::create(['id_persona_convocatoria' => $proceso->id, 'num_registro' => $resultado[0]->max_num_registro+1,'estado'=> 0 ,]);
+                return response()->json(['message' => 'Se realizo el registro', 'persona' => $persona, 'proceso'=>$proceso,'num_registro'=>$evaluacion->num_registro,'provincia'=>$provincia,'flag'=>1]);
+            }
+        } 
+        else {
+            return response()->json(['message' => 'El postulante no petenece a la sede Regional', 'persona' => $persona, 'proceso'=>$proceso,'flag'=>1]);
+        }                
+}
     
     public function evaluar($dni){
         $persona = Persona::select('id','nombres','apellido_pat','apellido_mat','documento' )->where('documento',$dni)->get();
